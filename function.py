8166fea16f02474590ec9f1412ee7c679c802541
fangwk <fwk1010@github.com>
2017-12-24 21:34:50 +0800

    learn function

--- function.py
+++ function.py
@@ -8,3 +8,12 @@ def func(s):
 
 # use function
 func('hi,function!')
+
+# define a function with default parameters
+def func_default_paramters(name,age=18,country='China'):
+    print('name:%s'%name,'age:%d'%age,'country:%s'%country)
+
+# examples
+func_default_paramters('Hanmeimei')
+func_default_paramters('Lilei',19)
+func_default_paramters('Tom',country='USA')
