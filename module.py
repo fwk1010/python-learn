c55876985d9efdd39b5127279a127d63457595da
fangwk <fwk1010@github.com>
2017-12-25 21:46:17 +0800

    words fix

--- module.py
+++ module.py
@@ -2,10 +2,10 @@
 '''
 This file is to learn about module more detail.
 '''
-# Import a exits module,like time
+# Import an exist module,like time
 import time
 
-# Import a exits module partly,like pi in math
+# Import an exist module partly,like pi in math
 from math import pi
 
 print("localtime:",time.localtime())
